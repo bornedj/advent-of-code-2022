0f80546946ae39f3255f76fdcb406920214cc785
bornedj <bornedj@miamioh.edu>
2023-09-03 01:06:51 -0400

    feat: finished day one

--- calories/src/advent_1.rs
+++ calories/src/advent_1.rs
@@ -1,22 +1,16 @@
-use std::fs::read_to_string;
-
-// I believe this has to be O(n)
-fn main() {
+fn highest_calorie_count() {
     // read file
     let calories_input_contents =
         read_to_string("./calories/calories_input.txt").expect("failed to read file");
 
     // iter through throws on carriage. Keep running total of highest value
-    let mut highest_totals: Vec<u64> = vec![0; 3];
+    let mut highest_total: u64 = 0;
     let mut current_total: u64 = 0;
     let calories: Vec<&str> = calories_input_contents.split("\n").collect();
     for line in calories.iter() {
         // it's either a blank or a num
         if line == &"" {
-            if current_total > highest_totals[0] {
-                highest_totals[0] = current_total;
-                highest_totals.sort();
-            }
+            if current_total > highest_total {highest_total = current_total}
             current_total = 0;
         } else {
             let num = match line.parse::<u64>() {
@@ -26,5 +20,4 @@ fn main() {
             current_total += num;
         }
     }
-    println!("Highest total: {:?}", highest_totals.iter().sum::<u64>())
 }
